0c09cfc7786c990fb0962672240030d9011ee1d1
programmer-abdullah-qr <abdullahqadeer711@gmail.com>
2026-07-17 19:15:43 +0500

    i have completed Question

---
+++ Contact_app.py
@@ -0,0 +1,16 @@
+contacts = {
+    "Ali": {
+        "Phone": "03001234567",
+        "City": "Islamabad"
+    },
+    "Ahmed": {
+        "Phone": "031234567",
+        "City": "Rwp"
+    },
+    "Hamza": {
+        "Phone": "032234567",
+        "City": "lahore"
+    }
+}
+information = input("Enter contact name:")
+print("Your information is :",contacts[information])
